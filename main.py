ac15879f0fa2c7351524aaea58eec1dcaeef39ce
Sian Jose <sian3jose@gmail.com>
2024-10-22 11:07:11 -0400

    git add . git commit -m "add lab 6 main and encode() functions"

---
+++ main.py
@@ -0,0 +1,24 @@
+def encode(password):
+    password1 = ''
+    for i in password:
+        element = int(i) + 3
+        password1 += str(element)
+    print("Your password has been encoded and stored!")
+    return password1
+
+def decode():
+    pass
+
+if __name__ == '__main__':
+    option = 0
+    while option < 3:
+        print("Menu\n----------\n1. Encode\n2. Decode\n3. Quit")
+        option = int(input("\nPlease enter an option: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded = encode(password)
+        elif option == 2:
+            decode()
+        elif option == 3:
+            continue
