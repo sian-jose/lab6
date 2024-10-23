de13171cb2904c6ebc6ba8806ef3773e741ca737
Sian Jose <sian3jose@gmail.com>
2024-10-23 15:31:09 -0400

    fixed encode

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def encode(password):
     password1 = ''
     for i in password:
-        element = int(i) + 3
+        if i == "7":
+            element = "0"
+        elif i == "8":
+            element = "1"
+        elif i == "9":
+            element = "2"
+        else:
+            element = int(i) + 3
         password1 += str(element)
     print("Your password has been encoded and stored!")
     return password1
@@ -14,7 +21,6 @@ if __name__ == '__main__':
     while option < 3:
         print("Menu\n----------\n1. Encode\n2. Decode\n3. Quit")
         option = int(input("\nPlease enter an option: "))
-
         if option == 1:
             password = input("Please enter your password to encode: ")
             encoded = encode(password)
